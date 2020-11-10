d5f78501263c6130a8ddd013cfd268c3b6596867
zohan22 <66893240+zohan22@users.noreply.github.com>
2020-11-10 07:09:12 -0400

    agregando

---
+++ SEjemplo01/Ejemplo01/Principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "NOMBRES: SAMUEL DAYLER" << endl;
+	cout << "APELLIDOS: AMONZABEL GONZALES" << endl;
+	cout << "C.U: 135-4981" << endl;
+	cout << "C.I: 10391075" << endl;
+	cout << "CARRERA: INGENIERIA DE SISTEMAS" << endl;
+	cout << "CURSO: 4TO SEMESTRE" << endl;
+	system("pause");
+	return 0;
+}
